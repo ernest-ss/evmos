8b5407e8a285714f05f2e4c7a1ab15ae70784a17
shi wb <shiwb@shideMacBook-Pro.local>
2024-01-07 12:33:18 +0800

    change devops file

--- networks/cluster/material_gen.sh
+++ networks/cluster/material_gen.sh
@@ -1,13 +1,13 @@
 ### build dockerfile
 
-rm -rf ./chainData/tabi
-../../build/evmosd testnet init-files --v 8 -o ./chainData/tabi --keyring-backend=test --starting-ip-address 192.167.10.2 --chain-id evmos_9000-1
+rm -rf ./chainData/er
+../../build/evmosd testnet init-files --v 8 -o ./chainData/ernest --keyring-backend=test --starting-ip-address 192.167.10.2 --chain-id evmos_9000-1
 
   for i in {0..7}; do
-    APP_TOML="./chainData/tabi/node$i/evmosd/config/app.toml"
-    GENESIS="./chainData/tabi/node$i/evmosd/config/genesis.json"
-    TMP_GENESIS="./chainData/tabi/node$i/evmosd/config/tmp_genesis.json"
-    DATADIR="./chainData/tabi/node$i/evmosd/data"
+    APP_TOML="./chainData/ernest/node$i/evmosd/config/app.toml"
+    GENESIS="./chainData/ernest/node$i/evmosd/config/genesis.json"
+    TMP_GENESIS="./chainData/ernest/node$i/evmosd/config/tmp_genesis.json"
+    DATADIR="./chainData/ernest/node$i/evmosd/data"
 
   	# Set gas limit in genesis
 	  jq '.consensus_params["block"]["max_gas"]="30000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
